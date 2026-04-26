0a58ff57c8401c5b6ee9857728cd65e2c70ca4b8
Louise <louiseeclaughton@gmail.com>
2026-04-26 11:08:25 +0100

    added navigation

---
+++ app/components/navigation.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState } from 'react';
+import Link from 'next/link';
+
+export default function Navigation () {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+        <div className="fixed inset-0 flex justify-center h-20 z-99">
+            <button 
+                className={`absolute top-4 z-50 font-['General Sans'] rounded-3xl border border-black py-2 px-4 transition-colors hover:cursor-pointer
+                    ${open ? 'bg-[#E6E0DA] hover:bg-[#F0F0F0]' : 'bg-[#F0F0F0] hover:bg-[#E6E0DA]'}
+                `}
+                onClick={() => setOpen(!open)}
+            >
+                MENU
+            </button>
+        </div>
+
+        <div
+            className={`absolute inset-0 flex items-center justify-center bg-[#E6E0DA]
+            transition-all duration-500 -translate-y-full
+            ${open 
+                ? 'translate-y-0 pointer-events-auto' 
+                : '-translate-y-full pointer-events-none'
+            }`}
+        >
+            <div className="flex flex-col font-['General Sans'] font-bold text-8xl tracking-[-6px] leading-[110px] text-center">
+                <Link href="/about">About</Link>
+                <Link href="/projects">Projects</Link>
+                <Link href="/interests">Interests</Link>
+                <Link href="/contact">Contact</Link>
+            </div>
+        </div>
+    </>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -2,11 +2,17 @@ import { client } from "@/lib/contentful";
 import Card from "./components/card";
 import Projects from "./components/projects";
 
+import Navigation from "./components/navigation";
+
 export default async function Home() {
   return (
-    <main>
-      <h1>Projects</h1>
-      <Projects />
+    <main className="h-screen w-screen flex justify-center items-center">
+      <Navigation />
+      <h1 className="flex flex-col font-['General Sans'] font-bold text-8xl tracking-[-6px] leading-[110px] text-center hover:cursor-pointer">
+        <div><span className="hover:text-amber-400 transition-colors">Designer</span>,</div>
+        <div><span className="hover:text-purple-500 transition-colors">Developer</span>,</div>
+        <div><span className="hover:text-pink-400 transition-colors">Storyteller</span>.</div>
+      </h1>
     </main>
   );
 }
