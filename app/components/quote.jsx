f51e115312e737d74bdcbe0adc84809c3d2ae238
Louise <louise.claughton@flauntdigital.com>
2026-05-01 15:54:25 +0100

    fix to the hero having a bit of scroll overflow

--- app/components/quote.jsx
+++ app/components/quote.jsx
@@ -3,7 +3,7 @@ export default function Quote({ block }) {
 
   return (
     <div
-      className={`flex flex-col md:flex-row px-12 md:px-24 py-24 gap-2 md:gap-8 overflow-x-hidden relative ${fields?.textAlignment === "Center" ? "text-center items-center" : "text-left"}`}
+      className={`flex flex-col md:flex-row px-12 md:px-24 py-24 gap-2 md:gap-8 overflow-hidden relative ${fields?.textAlignment === "Center" ? "text-center items-center" : "text-left"}`}
     >
       <div className="cloud w-[350px] h-[120px] bg-[#f2f9fe] rounded-full absolute mt-[120px] mx-auto mb-[20px] z-1 opacity-60 bottom-[30%] right-[-5%]"></div>
       <span className="font-bold text-3xl md:text-5xl">"</span>
