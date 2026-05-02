1764fba67f193cd5d46bb04fec5aa99035fa9321
Louise <louiseeclaughton@gmail.com>
2026-05-02 08:50:27 +0100

    fixed a cloud that was getting cut off

--- app/components/quote.jsx
+++ app/components/quote.jsx
@@ -5,7 +5,7 @@ export default function Quote({ block }) {
     <div
       className={`flex flex-col md:flex-row px-12 md:px-24 py-24 gap-2 md:gap-8 overflow-hidden relative ${fields?.textAlignment === "Center" ? "text-center items-center" : "text-left"}`}
     >
-      <div className="cloud w-[350px] h-[120px] bg-[#f2f9fe] rounded-full absolute mt-[120px] mx-auto mb-[20px] z-1 opacity-60 bottom-[30%] right-[-5%]"></div>
+      <div className="cloud w-[350px] h-[120px] bg-[#f2f9fe] rounded-full absolute mt-[120px] mx-auto mb-[20px] z-1 opacity-60 bottom-[15%] right-[-5%]"></div>
       <span className="font-bold text-3xl md:text-5xl">"</span>
       <div className="flex flex-col">
         <div className="mb-4 z-1">{fields?.content}</div>
