80ebd25b27f44ac1afc30134a462ea2514b1673c
Louise <louise.claughton@flauntdigital.com>
2026-07-10 14:04:04 +0100

    add a filter to the blog

--- app/components/blog.jsx
+++ app/components/blog.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { fadeInView } from "@/lib/scrollFade";
 import BlogCard from "./blogCard";
 
@@ -34,16 +34,55 @@ function BlogItem({ post, index, grid }) {
 }
 
 export default function Blog({ grid = true, posts = [] }) {
+    const [selectedTag, setSelectedTag] = useState("All");
+
+    const tags = useMemo(() => {
+        const unique = [...new Set(posts.map(post => post.fields.tag))];
+        return ["All", ...unique];
+    }, [posts]);
+
+    const filteredPosts = useMemo(() => {
+        if (selectedTag === "All") return posts;
+
+        return posts.filter(
+            post => post.fields.tag === selectedTag
+        );
+    }, [posts, selectedTag]);
+
     return (
-        <div className={`py-12 ${grid ? 'flex flex-col md:grid md:grid-cols-2 gap-14 md:gap-28 px-12 md:px-24 pb-56' : 'flex flex-col gap-12 md:gap-20'}`}>
-            {posts.map((post, index) => (
-                <BlogItem
-                    key={post.sys.id}
-                    post={post}
-                    index={index}
-                    grid={grid}
-                />
-            ))}
-        </div>
+        <>
+            <div className="flex flex-wrap gap-3 px-12 md:px-24 pt-12 justify-center lg:justify-start">
+                {tags.map(tag => (
+                    <button
+                        key={tag}
+                        onClick={() => setSelectedTag(tag)}
+                        className={`caption rounded-full px-4 py-2 border-2 hover:cursor-pointer transition ${
+                            selectedTag === tag
+                                ? "bg-black text-white"
+                                : "text-black"
+                        }`}
+                    >
+                        {tag}
+                    </button>
+                ))}
+            </div>
+
+            <div
+                className={`py-12 ${
+                    grid
+                        ? "flex flex-col md:grid md:grid-cols-2 gap-14 md:gap-28 px-12 md:px-24 pb-56"
+                        : "flex flex-col gap-12 md:gap-20"
+                }`}
+            >
+                {filteredPosts.map((post, index) => (
+                    <BlogItem
+                        key={post.sys.id}
+                        post={post}
+                        index={index}
+                        grid={grid}
+                    />
+                ))}
+            </div>
+        </>
     );
 }
