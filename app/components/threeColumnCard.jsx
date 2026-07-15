73debfbced197d3c7b46e8c616042c9d3bd2d1c8
Louise <louise.claughton@flauntdigital.com>
2026-07-15 14:43:40 +0100

    3 col block fix for less content

--- app/components/threeColumnCard.jsx
+++ app/components/threeColumnCard.jsx
@@ -3,14 +3,14 @@ export default function ThreeColumnCard({ block, id }) {
 
   return (
     <div
-      className="flex flex-col justify-center text-center py-12 md:py-24"
+      className="flex flex-col justify-center text-center pt-12 pb-24"
     >
       <h2 className="font-bold text-3xl md:text-4xl tracking-[-2px] leading-[80px] md:leading-[110px]">
         {fields?.blockTitle}
       </h2>
       <div
         id={id}
-        className="w-screen flex flex-col md:grid md:grid-cols-3 gap-12 md:gap-24 px-12 md:px-24 py-12"
+        className="w-screen flex flex-col max-w-full md:grid md:grid-cols-3 gap-12 md:gap-24 px-12 md:px-24"
       >
         <div className="flex flex-col gap-4">
           <span className="font-bold text-xl">{fields?.column1Title}</span>
